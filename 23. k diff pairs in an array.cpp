2b5a8e690409702ef94a3659bdf69cccf0c01f6b
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2023-11-29 17:05:39 +0530

    Update 23. k diff pairs in an array.cpp

--- 23. k diff pairs in an array.cpp
+++ 23. k diff pairs in an array.cpp
@@ -1,12 +1,10 @@
 // https://leetcode.com/problems/k-diff-pairs-in-an-array
 
 class Solution {
-public:
-    int findPairs(vector<int>& nums, int k) {
+    int mapMethod(vector<int> nums, int k) {
         unordered_map<int,int> mp;
         for(int i: nums)
             mp[i]++;
-
         int res = 0;
         for(auto x: mp) {
             if(k == 0) {
@@ -17,5 +15,30 @@ public:
                 res++;
         }
         return res;
+    }
+public:
+    int findPairs(vector<int>& nums, int k) {
+        return mapMethod(nums, k);
+        // int n = nums.size();
+        // int i = 0, j = 1;
+        // int res = 0;
+        // sort(nums.begin(), nums.end());
+
+        // while (i < n && j < n) {
+        //     if (nums[j] - nums[i] == k) {
+        //         res++;
+        //         i++;
+        //         j++;
+        //         while(j < n && nums[j] == nums[j-1])
+        //             j++;
+        //     } else if (nums[j] - nums[i] > k) {
+        //         i++;
+        //         if (j - i == 0)
+        //             j++;
+        //     } else {
+        //         j++;
+        //     }
+        // }
+        // return res;
     }
 };
