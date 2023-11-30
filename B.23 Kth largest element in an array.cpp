f1022dfbf7e6ede9d63b2544c6413b4b0853e729
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2023-11-30 13:28:21 +0530

    Created B.23 Kth largest element in an array.cpp

---
+++ B.23 Kth largest element in an array.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int i: nums)
+            pq.push(i);
+        while(--k)
+            pq.pop();
+        return pq.top();
+    }
+};
