14193627eb609540aa5436cbd38f0d4dfeca6b49
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2023-12-11 09:14:14 +0530

    Created B.43 Count number of teams.cpp

---
+++ B.43 Count number of teams.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/count-number-of-teams
+
+class Solution {
+public:
+        int numTeams(vector<int>& rating) {
+        int n = rating.size(), ans=0;
+        vector<int> greater = vector<int>(n,0);
+        vector<int> smaller = vector<int>(n,0);
+        for(int j = 0;j<n;j++) {
+            for(int k = j+1;k<n; k++) {
+                if(rating[j]<rating[k]) smaller[j]++;
+                if(rating[j]>rating[k]) greater[j]++;
+            }
+        }
+        for(int i = 0; i<n; i++) {
+            for(int j = i+1;j<n;j++) {
+                if(rating[i]==rating[j]) continue;
+                if(rating[i]<rating[j]) {
+                    //for(int k = j+1;k<n;k++) ans+=(rating[j]<rating[k]);
+                    ans+=smaller[j];
+                } else {
+                    //for(int k = j+1;k<n;k++) ans+=(rating[j]>rating[k]);
+                    ans+=greater[j];
+                }
+            }
+        }
+        return ans;
+    }
+};
